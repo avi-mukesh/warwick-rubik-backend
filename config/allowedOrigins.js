54f81150bc8d36f6f1444edcd413e661c802cd7f
Avi Mukesh <avimukesh10@gmail.com>
2022-09-25 21:25:32 +0100

    added rubik.avimukesh.com domain

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     "https://warwick-rubiks.web.app",
-    "http://localhost:3000", // comment out for production
+    "https://rubik.avimukesh.com",
+    // "http://localhost:3000", // comment out for production
 ]
 
 module.exports = allowedOrigins
