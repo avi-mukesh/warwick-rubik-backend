93f66d46b8d3be2a924681d1fedadd10c8567aed
Avi Mukesh <avimukesh10@gmail.com>
2022-09-25 16:13:09 +0100

    dotenv is required

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-    "https://warwick-rubiks.web.app",
-    "http://localhost:3000",
+    "https://warwick-rubik.web.app",
+    // "http://localhost:3000",
 ]
 
 module.exports = allowedOrigins
