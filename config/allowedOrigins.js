36b86e471189cafd6d4e8ad7af0488363b37a498
Avi Mukesh <avimukesh10@gmail.com>
2022-09-26 12:38:48 +0100

    removed hyphen from domain

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    "https://warwick-rubik.web.app",
+    "https://warwickrubik.web.app",
     "https://rubik.avimukesh.com",
     // "http://localhost:3000", // comment out for production
 ]
